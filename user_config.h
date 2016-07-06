00d949cba6da613552ee38848db15f61718d7160
Franz Levin <frazz@kryo.se>
2016-07-06 09:54:02 +0200

    Fixed typo in license

--- user_config.h
+++ user_config.h
@@ -2,7 +2,7 @@
 "THE BEER/MATE-WARE LICENSE":
 <xil@xil.se> wrote this file. As long as you retain this notice you
 can do whatever you want with this stuff. If we meet some day, and you think
-this stuff is worth it, you can buy us a ( > 0 ) beer/mate in return - The Xil TEAM#include "osapi.h"
+this stuff is worth it, you can buy us a ( > 0 ) beer/mate in return - The Xil TEAM
 */
 
 #ifndef USER_CONFIG_H
