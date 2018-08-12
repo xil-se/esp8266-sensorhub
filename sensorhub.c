d8281bebdd8cd779bbe002f4e0260f90614e0908
Franz Levin <frazz@kryo.se>
2018-08-12 22:37:09 +0200

    sensorhub: version 3

--- sensorhub.c
+++ sensorhub.c
@@ -19,7 +19,7 @@ this stuff is worth it, you can buy us a ( > 0 ) beer/mate in return - The Xil T
 
 #include "drivers/drivers.h"
 
-#define SENSORHUB_VERSION           2
+#define SENSORHUB_VERSION           3
 
 #define SENSOR_TASK_PRIO            2
 #define SENSOR_TASK_QUEUE_SIZE      1
